a361a0dfd010a84199e57b2341125d8ecc29a320
Jacob Stucky <jacobdstucky@gmail.com>
2026-05-15 22:19:28 -0400

    Made firebase secrets actually secret

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB63-pK0kObmbvZ05EhoccGZYXlfZPCJpc",
-  authDomain: "tornagator.firebaseapp.com",
-  projectId: "tornagator",
-  storageBucket: "tornagator.firebasestorage.app",
-  messagingSenderId: "841894955524",
-  appId: "1:841894955524:web:3a8180b19ac31dd98d8351"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
