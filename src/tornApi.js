da4f0d0ba533a0d4df1f52eec18a1c34584ba867
Jacob Stucky <241853601+Stuckzilla314@users.noreply.github.com>
2026-06-12 14:00:46 -0400

    upgrades not working on faction api call

--- src/tornApi.js
+++ src/tornApi.js
@@ -32,7 +32,7 @@ export const fetchUserData = async (apiKey, selections = 'basic,profile') => {
  */
 export const fetchFactionData = async (apiKey) => {
   try {
-    const url = `${BASE_URL}/faction/?selections=basic,rankedwars,chain,upgrades&key=${apiKey}`;
+    const url = `${BASE_URL}/faction/?selections=basic,rankedwars,chain&key=${apiKey}`;
     console.log('[TORNagator API] Fetching faction data from URL:', url.replace(apiKey, 'HIDDEN_KEY'));
     const response = await fetch(url);
     const data = await response.json();
